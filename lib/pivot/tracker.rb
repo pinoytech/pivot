dc788aa2e5f175c64736983e98e06fce25c7fa02
pinoytech <64101+pinoytech@users.noreply.github.com>
2020-11-03 02:19:05 +0800

    Working class for Pivotal::Tracker
    * count
    * items_for
    * pivoted?
    * total_points
    * unique_assignees

--- lib/pivot/tracker.rb
+++ lib/pivot/tracker.rb
@@ -1,5 +1,27 @@
 module Pivot
   class Tracker
-    # Code here!
+    class << self
+      def count(items)
+        items.count
+      end
+
+      def item_for(items, email)
+        items.last { |item| item[:assignee] == email }
+      end
+
+      def pivoted?(items, email)
+        items.any? { |item| item[:assignee] == email }
+      end
+
+      def total_points(items, assignee: nil)
+        return items.reverse.uniq { |item| item[:assignee] }.sum { |item| item[:points] } if assignee.nil?
+
+        items.reverse.select { |item| item[:assignee] == assignee }.sum { |item| item[:points] }
+      end
+
+      def unique_assignees(items)
+        items.uniq { |item| item[:assignee] }
+      end
+    end
   end
 end
