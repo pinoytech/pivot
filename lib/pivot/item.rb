cb7465a365413b4390ee4a5b2ccbebd82823ebc3
pinoytech <64101+pinoytech@users.noreply.github.com>
2020-11-03 02:46:00 +0800

    Working class for Pivotal::Item
    * +
    * project_code
    * valid?

--- lib/pivot/item.rb
+++ lib/pivot/item.rb
@@ -1,5 +1,25 @@
 module Pivot
   class Item
-    # Code here!
+    attr_reader :name, :assignee, :points
+
+    VALID_PROJECT_CODES = %w[EREC AZR].freeze
+
+    def initialize(name:, assignee:, points:)
+      @name = name
+      @assignee = assignee
+      @points = points
+    end
+
+    def +(other)
+      points + other.points
+    end
+
+    def project_code
+      name.gsub(/-\d*/, '')
+    end
+
+    def valid?
+      VALID_PROJECT_CODES.include?(project_code)
+    end
   end
 end
